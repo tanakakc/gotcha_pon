4e2e9ecae67c50d3c852e8210f938624fc2a807e
Keishi Tanaka <k-tanaka@ruby-dev.jp>
2025-11-23 08:32:35 +0900

    feat: implement basic gacha logic with random ID sampling

--- test/ponnable_test.rb
+++ test/ponnable_test.rb
@@ -5,38 +5,43 @@ require_relative "test_helper"
 class PonnableTest < Minitest::Test
   def setup
     @model_class = Class.new(TestItem)
+    TestItem.delete_all  # Clean up test data
   end
 
   def test_includes_ponnable_concern
     assert @model_class.include?(GachaPon::Ponnable)
   end
 
-  def test_has_gacha_pon_configuration_attribute
-    assert_respond_to @model_class, :gacha_pon_configuration
-  end
-
-  def test_gacha_pon_config_method_exists
-    assert_respond_to @model_class, :gacha_pon_config
-  end
-
   def test_gacha_pon_method_exists
     assert_respond_to @model_class, :gacha_pon
   end
 
-  def test_gacha_pon_config_sets_configuration
-    @model_class.gacha_pon_config(
-      rules: { test: [] },
-      default_rule: :test
-    )
+  def test_gacha_pon_returns_query_for_single_item
+    # Create test items
+    3.times { |i| TestItem.create!(name: "item_#{i}") }
 
-    config = @model_class.gacha_pon_configuration
-    assert_equal :test, config[:default_rule]
-    assert_equal({ test: [] }, config[:rules])
+    query = TestItem.gacha_pon(user: "test_user")
+    assert_respond_to query, :first
+
+    result = query.first
+    assert_instance_of TestItem, result
   end
 
-  def test_gacha_pon_method_raises_not_implemented
-    assert_raises NotImplementedError do
-      @model_class.gacha_pon(user: "test_user")
-    end
+  def test_gacha_pon_returns_query_for_multiple_count
+    # Create test items
+    5.times { |i| TestItem.create!(name: "item_#{i}") }
+
+    query = TestItem.gacha_pon(user: "test_user", count: 3)
+    results = query.to_a
+
+    assert_instance_of Array, results
+    assert_equal 3, results.length
+    results.each { |item| assert_instance_of TestItem, item }
+  end
+
+  def test_gacha_pon_returns_empty_when_no_items
+    query = TestItem.gacha_pon(user: "test_user")
+    result = query.first
+    assert_nil result
   end
 end
