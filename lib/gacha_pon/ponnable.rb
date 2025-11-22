c416c3bf82caea9238fc6b75afc270aa0c15d540
Keishi Tanaka <k-tanaka@ruby-dev.jp>
2025-11-23 08:32:35 +0900

    feat: implement basic gacha logic with random ID sampling

--- lib/gacha_pon/ponnable.rb
+++ lib/gacha_pon/ponnable.rb
@@ -6,29 +6,10 @@ module GachaPon
   module Ponnable
     extend ActiveSupport::Concern
 
-    included do
-      # Configure gacha settings for this model
-      class_attribute :gacha_pon_configuration, default: {}
-    end
-
     class_methods do
-      # Configure gacha rules and settings
-      def gacha_pon_config(options = {})
-        default_config = {
-          weight_column: nil,
-          default_rule: :standard,
-          rules: {}
-        }
-
-        self.gacha_pon_configuration = default_config.merge(options)
-      end
-
-      # Execute gacha (main method)
-      def gacha_pon(user:, count: 1, rule: nil)
-        rule ||= gacha_pon_configuration[:default_rule]
-
-        # TODO: Implement gacha logic
-        raise NotImplementedError, "gacha_pon method not implemented yet"
+      # Execute gacha - returns a query for random selection
+      def gacha_pon(user:, count: 1)
+        where(id: pluck(:id).sample(count))
       end
     end
   end
